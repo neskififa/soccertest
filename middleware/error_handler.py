c0c8a7466fe0cf5ab0f0a651fbd8cd61a2f9c8b7
neskififa <ggandrade2013@gmail.com>
2026-03-14 13:23:02 -0300

    Create error_handler.py

---
+++ middleware/error_handler.py
@@ -0,0 +1,21 @@
+from flask import jsonify
+
+
+def register_error_handlers(app):
+
+    @app.errorhandler(404)
+    def not_found(error):
+
+        return jsonify({
+            "status": "error",
+            "message": "route not found"
+        }), 404
+
+
+    @app.errorhandler(500)
+    def internal_error(error):
+
+        return jsonify({
+            "status": "error",
+            "message": "internal server error"
+        }), 500
